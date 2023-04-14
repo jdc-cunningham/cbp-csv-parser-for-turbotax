1ee8fabe24502b45196e5822d721b8578f442e73
Jacob Cunningham <jcun4128@gmail.com>
2023-04-14 16:15:26 -0500

    start working on new parser

--- standalone-parser/index.js
+++ standalone-parser/index.js
@@ -1,5 +1,57 @@
 const fs = require("fs");
+const http = require('http');
 
-fs.readFile("2021-account-statement.csv", "utf-8", (err, data) => {
-  console.log(data);
-});
+function buildHtml(rows) {
+  var header = '';
+  var body = '';
+
+  Object.keys(rows).forEach(txTime => {
+    const row = rows[txTime];
+
+    let bgColor = '';
+
+    if (row.type === 'deposit') {
+      bgColor = '#FFFF8F';
+    }
+
+    body += `<div style="background-color: ${bgColor}">${txTime}</div>`;
+  });
+
+  return '<!DOCTYPE html>'
+    + '<html><head>' + header + '</head><body>' + body + '</body></html>';
+};
+
+http.createServer(function (req, res) {
+  let html = "";
+
+  const csv2021Rows = {};
+
+  fs.readFile("2021-account-statement.csv", "utf-8", (err, data) => {
+    const rows = data.split('\n');
+
+    rows.shift(); // remove headers
+
+    rows.forEach(row => {
+      if (row.length) {
+        const cols = row.split(',');
+        const type = cols[1];
+        const time = cols[2];
+
+        csv2021Rows[time] = {
+          cols,
+          type
+        }
+      }
+    });
+
+    html = buildHtml(csv2021Rows);
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Content-Length': html.length,
+      'Expires': new Date().toUTCString()
+    });
+
+    res.end(html);
+  });
+}).listen(8080);
